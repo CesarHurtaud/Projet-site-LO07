d5a4e860b083f83bf1d1d1af6aef45a4a4b60668
Ganstrich <adam.benjelloun@gmail.com>
2017-05-02 16:47:50 +0200

    Ajout de la classe etudiant.php

---
+++ data/classes/etudiant.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 29/04/17
+ * Time: 14:53
+ */
+class etudiant
+{
+    private $numero;
+    private $nom;
+    private $prenom;
+    private $admission;
+    private $filiere;
+
+    /**
+     * etudiant constructor.
+     * @param $numero
+     * @param $nom
+     * @param $prenom
+     * @param $admission
+     * @param $filiere
+     */
+    public function __construct($numero, $nom, $prenom, $admission, $filiere)
+    {
+        $this->setNumero($numero);
+        $this->setNom($nom);
+        $this->setPrenom($prenom);
+        $this->setAdmission($admission);
+        $this->setFiliere($filiere);
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * @param numero etudiant
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdmission()
+    {
+        return $this->admission;
+    }
+
+    /**
+     * @param string "TC" or "BR"
+     */
+    public function setAdmission($admission)
+    {
+        if(strcasecmp($admission, "TC") == 0)
+            $this->admission = "TC";
+        elseif(strcasecmp($admission, "BR") == 0)
+            $this->admission = "BR";
+        else {
+            $this->admission = 0;
+            echo("Erreur setAdmission");
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFiliere()
+    {
+        return $this->filiere;
+    }
+
+    /**
+     * @param mixed $filiere
+     */
+    public function setFiliere($filiere)
+    {
+        if(strcasecmp($filiere, "?") == 0)
+            $this->filiere = "?";
+        elseif(strcasecmp($filiere, "MPL") == 0)
+            $this->filiere = "MPL";
+        elseif(strcasecmp($filiere, "MSI") == 0)
+            $this->filiere = "MSI";
+        elseif(strcasecmp($filiere, "MRI") == 0)
+            $this->filiere = "MRI";
+        elseif(strcasecmp($filiere, "LIB") == 0)
+            $this->filiere = "LIB";
+        else {
+            $this->filiere = 0;
+            echo("Erreur setFiliere");
+
+        }
+    }
+
+
+
+}
